b9ca0c24ef8c6707890da7539de7e1d34bd96159
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2024-05-12 15:02:34 +0200

    Add proxy option

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ from scraper.scraper.twitter_scraper import Twitter_Scraper
 
 args = sys.argv
 
-if len(args) != 4:
-    print('Use like this : python main.py <mail> <username> <password>')
+if len(args) != 4 and len(args) != 5:
+    print('Use like this : python main.py <mail> <username> <password> [proxy]')
     sys.exit()
+
+proxy = args[4] if len(args) == 5 else None
 
 scraper = Twitter_Scraper(
     mail=args[1],
     username=args[2],
     password=args[3],
+    proxy=proxy
 )
 
 print(scraper)
